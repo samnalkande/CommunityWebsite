a1b74223393656430a78cd1588a98de6a5956ff1
Tim Bohlender <tim@tb-dev.de>
2020-01-29 20:39:09 +0100

    Update Page.cs

--- Domain/Page.cs
+++ Domain/Page.cs
@@ -1,9 +1,16 @@
-﻿namespace de.tbdev.CommunityWebsite.Domain
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace de.tbdev.CommunityWebsite.Domain
 {
     public class Page
     {
         public int Id { get; set; }
+        
+        [Required]
+        [StringLength(250)]
         public string Title { get; set; }
+        
+        [Required]
         public string Content { get; set; }
     }
 }
